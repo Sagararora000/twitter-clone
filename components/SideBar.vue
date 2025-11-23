93f3e78fc6139e78762502b133e27eee783f64ea
Sagar <sag28.01.2001@gmail.com>
2025-11-23 12:54:23 +0530

    feat: add ExploreBar, MainContent, and SideBar components; update layout in index.vue; add nuxt-icon dependency

---
+++ components/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="sideBarContainer">
+        <div class="sideItems">
+            <div class="logo">
+                <img
+                    src="../public/logo.png"
+                    alt="Logo"
+                    width="25px"
+                    height="25px"
+                />
+            </div>
+            <div class="menuItems">
+                <ul class="menuList">
+                    <li class="menuItem"></li>
+                    <li class="menuItem">Explore</li>
+                    <li class="menuItem">Notifications</li>
+                    <li class="menuItem">Messages</li>
+                    <li class="menuItem">Bookmarks</li>
+                    <li class="menuItem">Communities</li>
+                    <li class="menuItem">Premium</li>
+                    <li class="menuItem">Profile</li>
+                    <li class="menuItem">More</li>
+                    <button>Post</button>
+                </ul>
+            </div>
+        </div>
+        <div class="sideFooter"></div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="flex">
-        <SideBar />
-        <MainContent />
-        <ExploreBar />
+    <div class="flex flex-row justify-center items-center">
+        <div class="flex flex-row justify-evenly w-full max-w-[1200px]">
+            <div class="hidden md:block max-w-[250px]">
+                <SideBar />
+            </div>
+            <div class="block max-w-[600px]">
+                <MainContent />
+            </div>
+            <div class="hidden lg:block max-w-[350px]">
+                <ExploreBar />
+            </div>
+        </div>
     </div>
 </template>
 
